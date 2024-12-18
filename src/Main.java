a3292a8269e9978cd7abd5a46b8c5d7eba0317cc
chrillah <ch.ronnberg@gmail.com>
2024-12-18 10:44:26 +0100

    Add

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,45 @@ public class Main {
 
     public static void validate(){
         Scanner input = new Scanner(System.in);
+
+        /*
         System.out.println("Input first name: ");
         String fName = input.nextLine().trim();
+         */
+
+        boolean validated = false;
+
+        while(!validated){
+            System.out.println("Input first name: ");
+            String fName = "";
+            fName = input.nextLine().trim();
+            int checkNumbers = 0;
+            int checkLetters = 0;
+
+            for(int i = 0; i < fName.length(); i++){
+                if(!Character.isDigit(fName.charAt(i))) {
+                    System.out.println("Good");
+                } else {
+                    System.out.println("No diggity");
+                    checkNumbers++;
+                }
+            }
+            if(checkNumbers <= 0){
+                validated = true;
+            } else {
+                System.out.println("No po");
+            }
+        }
+
+
+
+
+        /*
         System.out.println("Input last name: ");
         String lName = input.nextLine().trim();
 
 
         System.out.println(fName+" "+lName);
+         */
     }
 }
