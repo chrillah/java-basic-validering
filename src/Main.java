7e51d9590d6ede9aac766c44f4dbe7cf001f18dc
chrillah <ch.ronnberg@gmail.com>
2024-12-18 10:04:46 +0100

    Add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
+        // Global variables
+        String fName;
+        String lName;
+        String address;
+        int streetNumber;
+        int socialNumber;
+
+        validate();
+
+    }
+
+    public static void validate(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input first name: ");
+        String fName = input.nextLine().trim();
+        System.out.println("Input last name: ");
+        String lName = input.nextLine().trim();
+
+
+        System.out.println(fName+" "+lName);
     }
 }
