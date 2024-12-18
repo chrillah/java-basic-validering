6476bad79905b94a3f04976ceb692ed47be140ed
chrillah <ch.ronnberg@gmail.com>
2024-12-18 17:09:21 +0100

    Add

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,88 +9,156 @@ public class Main {
         String fName;
         String lName;
         String address;
-        int streetNumber;
-        int socialNumber;
+        String streetNumber;
+        String socialNumber;
 
-        validate();
+        Scanner userInput = new Scanner(System.in);
 
-    }
-
-    public static void validate(){
-        Scanner input = new Scanner(System.in);
-/*
-        int choice = 0;
-        Scanner menu = new Scanner(System.in);
+        /*
+        do{
+            System.out.println("Enter first name: ");
+            fName = userInput.nextLine().trim();
+        }
+        while (!validateName(fName));
 
         do{
-            try {
-                choice = 0;
-                System.out.println("MENU BITCH");
-                System.out.println("1) Option 1");
-                System.out.println("2) Option 2");
-                System.out.println("3) Exit 3");
-                System.out.println("Enter your choice bitch");
-                choice = menu.nextInt();
-
-                switch(choice){
-                    case 1 -> System.out.println("Choice 1");
-                    case 2 -> System.out.println("Choice 2");
-                    case 3 -> System.out.println("Exiting bitch");
-                    default -> System.out.println("Invalid choice bitch try again");
-                }
-            } catch(InputMismatchException e){
-                System.out.println("No po, otra vez");
-                menu.nextLine();
-            }
-
-
-        } while(choice != 3);
-*/
-
-        String fName = "";
-        String lName = "";
-        boolean validated = false;
-
-        // Check first name
-        while(!validated){
-            System.out.println("Input first name: ");
-            fName = input.nextLine().trim();
-            validated = check(fName);
+            System.out.println("Enter last name: ");
+            lName = userInput.nextLine().trim();
         }
+        while (!validateName(lName));
 
-        // reset validation
-        validated = false;
-
-        // check last name
-        while(!validated){
-            System.out.println("Input last name: ");
-            lName = input.nextLine().trim();
-            validated = check(lName);
-        }
         System.out.println(fName + " " + lName);
+         */
+
+        /*
+        do{
+            System.out.println("Enter street name");
+            address = userInput.nextLine();
+        }
+        while(!validateAddress(address));
+
+        System.out.println(address);
+
+         */
+
+        /*
+        do{
+            System.out.println("Enter street number");
+            streetNumber = userInput.nextLine().trim();
+        }
+        while(!validateStreetNumber(streetNumber));
+        System.out.println(streetNumber);
+         */
+
+
+        do{
+            System.out.println("Enter social number: ");
+            socialNumber = userInput.nextLine();
+        }
+        while(!validateSocialNumber(socialNumber));
+
+        System.out.println(socialNumber);
+
+
+
+
 
     }
 
-    public static boolean check(String name){
-        boolean validated = false;
+    public static boolean checkForStreck(String name){
+        int checkStreck = 0;
+
+        if(name.charAt(0) == '-' || name.charAt(name.length() -1 ) == '-'){
+            checkStreck++;
+        }
+
+        return checkStreck <= 0;
+    }
+
+    public static boolean checkLength(String name){
+        return name.length() >= 2;
+    }
+
+    public static boolean checkForNumbers(String text){
         int checkNumbers = 0;
-        int checkLetters = 0;
-        // int checkBindesStreck = 0;
 
-        for(int i = 0; i < name.length(); i++){
-            if(!Character.isDigit(name.charAt(i)) && name.charAt(0) != '-' && name.charAt(name.length() -1) != '-'){
-                checkLetters++;
-            }
-            else {
-                checkNumbers++;
+        for(int i = 0; i < text.length(); i++){
+           if(Character.isDigit(text.charAt(i))){
+               checkNumbers++;
+           }
+        }
+        return checkNumbers <= 0;
+    }
+
+    public static boolean checkDigits(String numbers){
+        if(!checkForLetters(numbers)){
+return false;
+        }
+        if(numbers.length() != 10){
+            System.out.println("Too short");
+            return false;
+        }
+        return true;
+    }
+    public static boolean checkForLetters(String numbers){
+        try{
+            Long.parseLong(numbers);
+            return true;
+        }
+        catch (Exception e){
+            System.out.println("No letters!");
+            return false;
+        }
+    }
+
+    public static boolean validateName(String name){
+        int check = 0;
+        if(!checkLength(name)){
+            System.out.println("Too short, must have at least 2 letters");
+            check++;
+        }
+        if(!checkForNumbers(name)){
+            System.out.println("No numbers, I don't care if you are Elon Snusk child");
+            check++;
+        }
+        if(!checkForStreck(name)){
+            System.out.println("What the heck! No streck!");
+            check++;
+        }
+        return check == 0;
+    }
+
+    public static boolean validateAddress(String address){
+        int check = 0;
+        if(!checkForNumbers(address)){
+            System.out.println("No numbers, I don't care if you are Elon Snusk child");
+            check++;
+        }
+        return check == 0;
+    }
+
+    public static boolean validateStreetNumber(String streetNumber){
+        int check = 0;
+        for (int i = 0; i < streetNumber.length() - 1; i++){
+            if(!Character.isDigit(streetNumber.charAt(i))){
+                check++;
             }
         }
-        if(checkNumbers <= 0 && checkLetters >= 2){
-            validated = true;
-        } else {
-            System.out.println("No po");
+        if(check > 0){
+            System.out.println("NOOO LETTERS!! You can have one letter at the end");
         }
 
-        return validated;
+        return check == 0;
+    }
+
+    public static boolean validateSocialNumber(String socialNumber){
+        if(checkDigits(socialNumber)) {
+            String firstDigit = socialNumber.substring(0,6);
+            String lastDigit = socialNumber.substring(6);
+            socialNumber = firstDigit+"-"+lastDigit;
+            System.out.println(socialNumber);
+            return true;
+        }
+        return false;
     }
 }
